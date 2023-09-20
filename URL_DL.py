13855fbf1ad8d1ca84c91a2d61147c82cd12cc7f
polubod <polubod@siue.edu>
2023-09-20 13:24:18 -0500

    commit 3

--- URL_DL.py
+++ URL_DL.py
@@ -1,27 +1,12 @@
-
-#import urllib.request
-# from urllib.request import urlopen, Request
-
-# url = input()
-# request = Request(url)
-# response = urlopen(request)
-# response = response.read()
-# print(response)
-
 import urllib.request
-from bs4 import BeautifulSoup
-url = input()
-urllib.request.urlretrieve(url)
- 
-file = open("text_file.txt", "r")
-contents = file.read()
-soup = BeautifulSoup(contents, 'html.parser')
- 
-f = open("test1.txt", "w")
- 
-# traverse paragraphs from soup
-for data in soup.find_all("p"):
-    sum = data.get_text()
-    f.writelines(sum)
- 
-f.close()
+import requests
+import urllib
+from urllib.request import urlopen
+import urllib3
+import sys
+
+file_url = str(sys.argv[1])
+#print(file_url)
+with open('readme.txt', 'w') as f:
+    for line in urllib.request.urlopen(file_url):
+        f.write(line.decode('utf-8'))
