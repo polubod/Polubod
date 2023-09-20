9574f7477a3dafee30d8e5570fe0219778e5ca1e
polubod <polubod@siue.edu>
2023-09-19 22:52:33 -0500

    commit 2

---
+++ URL_DL.py
@@ -0,0 +1,26 @@
+
+#import urllib.request
+# from urllib.request import urlopen, Request
+
+# url = input()
+# request = Request(url)
+# response = urlopen(request)
+# response = response.read()
+# print(response)
+
+import urllib.request
+from bs4 import BeautifulSoup
+urllib.request.urlretrieve('https://www.reddit.com/r/funny/comments/16brnzb/self_aware/')
+ 
+file = open("text_file.txt", "r")
+contents = file.read()
+soup = BeautifulSoup(contents, 'html.parser')
+ 
+f = open("test1.txt", "w")
+ 
+# traverse paragraphs from soup
+for data in soup.find_all("p"):
+    sum = data.get_text()
+    f.writelines(sum)
+ 
+f.close()
